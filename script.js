02ee80fc7b1b7a06a055a646c61dc32f8600ea8f
bolafale <tricke432yt@gmail.com>
2025-07-23 13:50:42 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const display = document.getElementById("res");
+function append(inp){
+    display.value += inp;
+}
+function deleteLast(){
+    display.value = display.value.slice(0, -1);
+}
+function clearCE(){
+    display.value = "";
+}
+function calc(){
+    display.value = eval(display.value.replace('÷', '/'));
+}
